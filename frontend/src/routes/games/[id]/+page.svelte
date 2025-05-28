5392ae0259b404c231eae55002b0a4b590875308
stew.chan@gmail.com <stew.chan@gmail.com>
2025-05-27 21:35:27 -0400

    Refactor game page component by removing unnecessary loading state display and simplifying the rendering logic for current scene text.

--- frontend/src/routes/games/[id]/+page.svelte
+++ frontend/src/routes/games/[id]/+page.svelte
@@ -2,14 +2,7 @@
   export let data: { id: string, currentScene: { text: string } }; // Update the type definition
 </script>
 
-<!-- {#if data}
-  <pre>{JSON.stringify(data, null, 2)}</pre>
-{:else}
-  <h1>Loading...</h1>
-{/if} -->
-
 {#if data}
-  <h1>{data.id}</h1>
   <h1>{data.currentScene.text}</h1>
 {:else}
   <h1>Loading...</h1>
--- frontend/src/routes/games/[id]/+page.ts
+++ frontend/src/routes/games/[id]/+page.ts
@@ -4,10 +4,5 @@ import { api } from '$lib/app/api'
 export const load: PageLoad = async ({ params }) => {
 	const res = await api.games({ id: params.id }).get()
 	if (res.status !== 200) throw new Error('Game not found')
-
-	if (!res.data?.currentScene) {
-		throw new Error('Invalid game data structure')
-	}
-	console.log(res.data?.id)
 	return res.data
 }
