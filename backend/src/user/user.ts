d2da0cdfa3cce0402abac38d44d1ed2e2e790e69
stew.chan@gmail.com <stew.chan@gmail.com>
2025-06-08 13:28:13 -0400

    Enhance user route by introducing a user schema for improved response validation. Update the '/guest' endpoint to include schema validation, ensuring consistent data structure in responses.

--- backend/src/user/user.ts
+++ backend/src/user/user.ts
@@ -1,17 +1,30 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { uniqueNamesGenerator, animals } from 'unique-names-generator'
 
+const userSchema = t.Object({
+  id: t.String(),
+  name: t.String(),
+})
+
 export const user = new Elysia({ prefix: '/user' })
+
   .state({
-    user: {} as Record<string, string>, // {username: password}
-    session: {} as Record<number, string>, // {sessionId: username}
+    user: {} as Record<string, string>, // {userId: userName}
+    session: {} as Record<number, string>, // {sessionId: userId}
   })
-  .get('/guest', ({ store: { user } }) => {
-    const id = crypto.randomUUID()
-    const name = uniqueNamesGenerator({
-      dictionaries: [animals],
-      length: 1,
-    })
-    user[id] = name
-    return { id, name }
-  })
+
+  .get(
+    '/guest',
+    ({ store: { user } }) => {
+      const id = crypto.randomUUID()
+      const name = uniqueNamesGenerator({
+        dictionaries: [animals],
+        length: 1,
+      })
+      user[id] = name
+      return { id, name }
+    },
+    {
+      response: userSchema,
+    }
+  )
